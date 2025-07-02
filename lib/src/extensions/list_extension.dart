4e23c580228290182bf986de9e545f3a1ce0c94a
Wellington de Souza Filho <wellingtonsbipo@gmail.com>
2025-07-02 17:39:25 -0500

    Add endsWithRepeatedSequence method to ListExtension for repeated sequence detection

--- lib/src/extensions/list_extension.dart
+++ lib/src/extensions/list_extension.dart
@@ -49,4 +49,46 @@ extension ListExtension<T> on List<T> {
     final element = removeAt(elementIndex);
     insert(newIndex, element);
   }
+
+  /// Returns a list of the last [sequences] elements that are repeated
+  /// with a range of [minSequenceRange] to [maxSequenceRange].
+  ///
+  /// If [maxSequenceRange] is not provided, it will look for the longest
+  /// repeated sequence until the end of the list.
+  ///
+  /// If the list is too short to contain the repeated sequence,
+  /// it will return null.
+  List<T>? endsWithRepeatedSequence({
+    int sequences = 2,
+    int minSequenceRange = 1,
+    int? maxSequenceRange,
+  }) {
+    assert(sequences > 1);
+    assert(minSequenceRange > 0);
+    assert(maxSequenceRange == null || maxSequenceRange >= minSequenceRange);
+    final list = this;
+    if (list.length < 2 || sequences <= 1) return null;
+    if (list.length < (sequences * minSequenceRange)) return null;
+
+    final found = <T>[];
+    for (var range = 1; range > 0; range++) {
+      if (maxSequenceRange != null && range >= maxSequenceRange) return null;
+      for (var rangeIndex = 1; rangeIndex <= range; rangeIndex++) {
+        final baseIndex = list.length - rangeIndex;
+        for (var sequence = 1; sequence < sequences; sequence++) {
+          final compareIndex = baseIndex - (sequence * range);
+          if (compareIndex < 0) return null;
+          if (list[baseIndex] != list[compareIndex]) {
+            found.clear();
+            sequence = sequences;
+            rangeIndex = range + 1;
+          } else if (sequence == sequences - 1) {
+            found.insert(0, list[baseIndex]);
+          }
+        }
+      }
+      if (found.isNotEmpty) return found;
+    }
+    return null;
+  }
 }
---
+++ test/src/extensions/list_extension_test.dart
@@ -0,0 +1,103 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:utils_well/src/extensions/list_extension.dart';
+
+void main() {
+  group('endsWithRepeatedSequence', () {
+    test('must return true', () {
+      var list = [1, 1];
+      expect(
+        list.endsWithRepeatedSequence(),
+        [1],
+      );
+
+      list = [1, 1, 1];
+      expect(
+        list.endsWithRepeatedSequence(sequences: 3),
+        [1],
+      );
+
+      list = [0, 1, 1, 1];
+      expect(
+        list.endsWithRepeatedSequence(sequences: 2),
+        [1],
+      );
+
+      list = [0, 2, 1, 2, 1, 2, 1];
+      expect(
+        list.endsWithRepeatedSequence(sequences: 3),
+        [2, 1],
+      );
+
+      list = _gen(2, 3, false);
+      expect(
+        list.endsWithRepeatedSequence(sequences: 2),
+        list.sublist(0, 3),
+      );
+      list = _gen(3, 4, true);
+      expect(
+        list.endsWithRepeatedSequence(sequences: 3),
+        list.sublist(0, 4),
+      );
+
+      list = _gen(4, 10, true);
+      expect(
+        list.endsWithRepeatedSequence(sequences: 3),
+        list.sublist(0, 10),
+      );
+    });
+
+    test('must return false', () {
+      expect(
+        () => [].endsWithRepeatedSequence(sequences: 1),
+        throwsAssertionError,
+      );
+
+      expect(
+        () => [].endsWithRepeatedSequence(minSequenceRange: 0),
+        throwsAssertionError,
+      );
+
+      expect(
+        () => [].endsWithRepeatedSequence(maxSequenceRange: 0),
+        throwsAssertionError,
+      );
+
+      expect(
+        [1].endsWithRepeatedSequence(),
+        isNull,
+      );
+
+      expect(
+        [1, 2, 1, 2].endsWithRepeatedSequence(minSequenceRange: 3),
+        isNull,
+      );
+
+      expect(
+        [1, 2, 1, 2].endsWithRepeatedSequence(maxSequenceRange: 1),
+        isNull,
+      );
+
+      expect(
+        [1, 1].endsWithRepeatedSequence(sequences: 3),
+        isNull,
+      );
+
+      expect(
+        [1, 1, 1, 0].endsWithRepeatedSequence(),
+        isNull,
+      );
+
+      expect(
+        [2, 0, 1, 0].endsWithRepeatedSequence(sequences: 2),
+        isNull,
+      );
+    });
+  });
+}
+
+List<int> _gen(int seq, int range, bool reversed) => [
+      for (var i = 0; i < seq; i++) ...[
+        if (!reversed) ...[for (var j = 0; j < range; j++) j],
+        if (reversed) ...[for (var j = range - 1; j >= 0; j--) j],
+      ]
+    ];
