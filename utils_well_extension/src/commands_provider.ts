aa640b0ebf53ceeea1eed01e82ffe78739f5baa7
Wellington de Souza Filho <wellingtonsbipo@gmail.com>
2025-12-31 14:22:02 -0500

    Enhance command configuration: add support for command folders and improve command execution flow in CommandsProvider.

--- utils_well_extension/src/commands_provider.ts
+++ utils_well_extension/src/commands_provider.ts
@@ -25,6 +25,8 @@ class Item extends vscode.TreeItem {
         return i;
     }
 
+    folders: string[] = [];
+
     get finalLabel(): string {
         return (this.label as string) ?? this.cmd;
     }
@@ -45,9 +47,10 @@ class CommandsProvider implements vscode.TreeDataProvider<Item> {
 
     private loadItems(refresh: boolean = false) {
         const config = vscode.workspace.getConfiguration(_extensionId);
-        const commands = config.get<Array<any>>('commands');
+        const commands = config.get<{ list: any[], config: any[] }>('commands');
+        if (!commands) return;
         this.items = [Item.featureCreator(), new Item('')];
-        for (let commandData of commands || []) {
+        for (let commandData of commands.list || []) {
             if (!commandData && typeof commandData !== 'object') continue;
             const cmd = commandData['command'];
             const label = commandData['label'];
@@ -55,7 +58,13 @@ class CommandsProvider implements vscode.TreeDataProvider<Item> {
             if (!cmd || typeof cmd !== 'string') continue;
             if (label && typeof label !== 'string') continue;
             if (ownTerminal && typeof ownTerminal !== 'boolean') continue;
-            this.items.push(new Item(cmd, label, ownTerminal));
+            const item = new Item(cmd, label, ownTerminal);
+            const itemConfig = commands.config?.find(c => c['label'] === item.finalLabel);
+            const folders = itemConfig && itemConfig['folders'];
+            if (folders && Array.isArray(folders)) {
+                item.folders = folders;
+            }
+            this.items.push(item);
         }
         if (refresh) this._onDidChangeTreeData.fire();
     }
@@ -80,23 +89,9 @@ class CommandsProvider implements vscode.TreeDataProvider<Item> {
     }
 }
 
-function _runCommand(treeView: CommandsProvider, item?: Item) {
+async function _runCommand(treeView: CommandsProvider, item?: Item) {
     if (!item) {
-        const cmds = [];
-        for (let e of treeView.items) {
-            if (e.contextValue !== `${_extensionId}.runTerminalCommand`) continue;
-            cmds.push({
-                label: e.finalLabel,
-                item: e
-            });
-        }
-        vscode.window.showQuickPick(
-            cmds,
-            {
-                placeHolder: 'Select a command to run',
-                canPickMany: false
-            }
-        ).then(selected => item = selected?.item);
+        item = await _getUserItemSelection('Select a command to run', treeView.items, item => item.finalLabel);
     }
 
     if (!item) return;
@@ -110,34 +105,57 @@ function _runCommand(treeView: CommandsProvider, item?: Item) {
         return _runOnTerminal(item, workspaceFolders[0].uri.fsPath);
     }
 
-    const dirs = workspaceFolders.map(folder => ({
-        label: path.basename(folder.uri.fsPath),
-        description: folder.uri.fsPath,
-        uri: folder.uri
-    }));
-
-    vscode.window.showQuickPick(
-        dirs,
-        {
-            placeHolder: 'Select a folder to run',
-            canPickMany: false
-        }
-    ).then(selected => {
-        if (!selected) return;
-        _runOnTerminal(item!, selected.uri.fsPath);
-    });
+    const folder = await _getUserItemSelection(
+        'Select a folder to run the command',
+        [...workspaceFolders],
+        f => path.basename(f.uri.fsPath)
+    );
+    if (!folder) return;
+    _runOnTerminal(item, folder.uri.fsPath);
 }
 
 function _copyCommand(item: Item) {
     vscode.env.clipboard.writeText(item.cmd);
-    vscode.window.showInformationMessage(`Command copied to clipboard: ${item.finalLabel}`);
+    vscode.window.showInformationMessage(`Command copied to clipboard: ${item.finalLabel} `);
 }
 
-function _runOnTerminal(item: Item, folder: string) {
-    const terminalName = item.ownTerminal ? `Utils Well - ${item.finalLabel} - ${folder}` : `Utils Well`;
+async function _runOnTerminal(item: Item, f: string) {
+    let folder = f;
+    if (item.folders.length > 0) {
+        const subFolder = await _getUserItemSelection(
+            'Select a subfolder to run the command',
+            item.folders,
+            subfolder => subfolder
+        );
+        if (!subFolder) return;
+        folder = path.join(folder, subFolder);
+    }
+    const terminalName = item.ownTerminal ? `Utils Well - ${item.finalLabel} - ${folder} ` : `Utils Well`;
     const existingTerminal = vscode.window.terminals.find(t => t.name === terminalName);
     const terminal = existingTerminal ?? vscode.window.createTerminal({ name: terminalName, cwd: folder });
     terminal.show(true);
-    if (existingTerminal) { terminal.sendText(`cd ${folder}`, true); }
+    if (existingTerminal) terminal.sendText(`cd ${folder} `, true);
     terminal.sendText(item.cmd, true);
+}
+
+async function _getUserItemSelection<T>(
+    description: string,
+    items: T[],
+    itemToLabel: (item: T) => string,
+): Promise<T | undefined> {
+    const selectionItem = [];
+    for (let item of items) {
+        selectionItem.push({
+            label: itemToLabel(item),
+            value: item
+        });
+    }
+    const item = await vscode.window.showQuickPick(
+        selectionItem,
+        {
+            placeHolder: description,
+            canPickMany: false
+        }
+    );
+    return item?.value;
 }
