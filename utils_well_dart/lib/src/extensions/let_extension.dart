395a521943d162d5541a9ed762e53bb0520d7737
Wellington de Souza Filho <wellingtonsbipo@gmail.com>
2026-02-12 13:38:38 -0500

    Refactor LetExtension to remove null safety checks and simplify let method

--- utils_well_dart/lib/src/extensions/let_extension.dart
+++ utils_well_dart/lib/src/extensions/let_extension.dart
@@ -1,6 +1,5 @@
-extension LetExtension<T extends Object> on T? {
-  TT? let<TT>(TT Function(T it) block) {
-    if (this == null) return null;
-    return block(this!);
+extension LetExtension<T extends Object> on T {
+  TT let<TT>(TT Function(T it) block) {
+    return block(this);
   }
 }
